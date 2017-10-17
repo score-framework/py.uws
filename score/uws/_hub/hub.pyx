5d88a02e7bbf5b78a7356aa48f7c5984b23d2578
Necdet Can Ateşman <can@strg.at>
2017-10-17 11:14:22 +0200

    Fix: Re-acquire gil during callback functions
    The callback functions on_connect, on_message, etc. were all used
    outside the scope of the GIL, which was leading to strange errors.

--- score/uws/_hub/hub.pyx
+++ score/uws/_hub/hub.pyx
@@ -132,7 +132,7 @@ ctypedef void* voidP
 cdef map[UwsWebSocketP, voidP] _connectionMap
 
 
-cdef void _on_connect_callback(void* data, UwsWebSocket *ws):
+cdef void _on_connect_callback(void* data, UwsWebSocket *ws) with gil:
     hub = (<Hub>data)
     client = Client(hub)
     client._c_ws = ws
@@ -142,7 +142,7 @@ cdef void _on_connect_callback(void* data, UwsWebSocket *ws):
 
 
 cdef void _on_disconnect_callback(void* data, UwsWebSocket *ws, int code,
-                                  char *message, size_t length):
+                                  char *message, size_t length) with gil:
     hub = (<Hub>data)
     client = <Client>(_connectionMap[ws])
     hub._on_disconnect(client, code, str(message[:length], 'ASCII'))
@@ -151,7 +151,7 @@ cdef void _on_disconnect_callback(void* data, UwsWebSocket *ws, int code,
 
 
 cdef void _on_message_callback(void* data, UwsWebSocket *ws,
-                               char *message, size_t length):
+                               char *message, size_t length) with gil:
     client = <Client>(_connectionMap[ws])
     hub = <Hub>(client.hub)
     hub._on_message(client, str(message[:length], 'UTF-8'))
