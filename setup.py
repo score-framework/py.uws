0c592beee7e4845e9a0db93b4bb77f5bdc4e9386
Necdet Can Ateşman <can@atesman.at>
2018-07-26 17:08:59 +0200

    Release: 0.0.3

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ if platform.system() == 'Darwin':
 
 setup(
     name='score.uws',
-    version='0.0.2',
+    version='0.0.3',
     description='HTTP handler of The SCORE Framework',
     long_description=README,
     author='strg.at',
